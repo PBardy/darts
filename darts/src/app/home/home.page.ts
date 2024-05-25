19657e71dc085551e55b12c704336970d131d154
PBardy <pbardy00@gmail.com>
2024-05-25 12:43:43 +0100

    Home screen

--- darts/src/app/home/home.page.ts
+++ darts/src/app/home/home.page.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import {
+  addOutline,
+  peopleOutline,
+  refreshOutline,
+  settingsOutline,
+  statsChartOutline,
+} from 'ionicons/icons';
+
+addIcons({
+  addOutline,
+  refreshOutline,
+  peopleOutline,
+  settingsOutline,
+  statsChartOutline,
+});
 
 @Component({
   templateUrl: 'home.page.html',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, RouterLink],
 })
 export class HomePage {}
