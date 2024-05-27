4a0545265e138be40e16fcb9f130850bf3f3ff38
PBardy <pbardy00@gmail.com>
2024-05-27 09:36:41 +0100

    Add keyboard input

--- src/app/games/game/game.page.ts
+++ src/app/games/game/game.page.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { KeyboardComponent } from '@components/keyboard/keyboard.component';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { ellipsisVerticalOutline } from 'ionicons/icons';
+
+addIcons({ ellipsisVerticalOutline });
 
 @Component({
   templateUrl: './game.page.html',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, KeyboardComponent],
 })
-export class GamePage {}
+export class GamePage {
+  onCancel() {}
+
+  onConfirm(score: number) {}
+}
