eccb205fb48ea48339c6d60ea76d4fd868d8a3ee
PBardy <pbardy00@gmail.com>
2024-05-28 08:58:37 +0100

    After game stats

--- src/app/games/game-stats/game-stats.page.ts
+++ src/app/games/game-stats/game-stats.page.ts
@@ -1,22 +1,24 @@
-import { CommonModule, KeyValuePipe } from '@angular/common';
-import { Component, computed, effect, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { GameService } from '@store/game';
-import { chain, groupBy } from 'underscore';
+import { Game, GameService } from '@store/game';
+import { Round } from '@store/round';
+import { chain, groupBy, last } from 'underscore';
 
 @Component({
   templateUrl: './game-stats.page.html',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, KeyValuePipe],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class GameStatsPage {
+  router = inject(Router);
   gameService = inject(GameService);
 
   gameStats = computed(() => {
     const game = this.gameService.game()!;
-    const rounds = game.history;
-    const sets = chain(rounds)
+    const sets = chain(game.history)
       .groupBy((round) => round.set)
       .map((set) => groupBy(set, (round) => round.leg))
       .value();
@@ -24,7 +26,25 @@ export class GameStatsPage {
     return sets;
   });
 
-  constructor() {
-    effect(() => console.log(this.gameStats()));
+  getSetWinner(game: Game, set: Round[]) {
+    const lastRound = last(set) as Round;
+    const [pid] = Object.entries(lastRound.scores).find(
+      ([, value]) => value.total === 0,
+    )!;
+
+    return game.players[pid];
+  }
+
+  getLegWinner(game: Game, leg: Round[]) {
+    const lastRound = last(leg) as Round;
+    const [pid] = Object.entries(lastRound.scores).find(
+      ([, value]) => value.total === 0,
+    )!;
+
+    return game.players[pid];
+  }
+
+  onContinue() {
+    this.router.navigate(['home']);
   }
 }
